31bac259f64cfa849860ca2314614517a28c48cf
Srabisto80 <120586224+Srabisto80@users.noreply.github.com>
2023-01-03 11:35:15 -0700

    "router and layout"

--- money-managed-app/Client/src/App.js
+++ money-managed-app/Client/src/App.js
@@ -1,10 +1,25 @@
-
 import './App.css';
+import Layout from './Components/Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/login/Login';
+import ForgotPass from './Components/forgotPass/ForgotPass';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+     <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+              <Route index element={<Login />} />
+              <Route path="ForgotPW" element={<ForgotPass />} />
+              {/* <Route path="ForgotPW" element={<ForgotPass />} />
+              <Route path="ForgotPW" element={<ForgotPass />} /> */}
+          </Route>
+      </Routes>
+  </BrowserRouter>
     </div>
   );
 }
