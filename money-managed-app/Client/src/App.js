3b00c98340ff80a7dc297dc4643f21e0f66455dd
Korra T <118988951+ZTeasley@users.noreply.github.com>
2023-01-04 08:14:56 -0800

    "Login page"

--- money-managed-app/Client/src/App.js
+++ money-managed-app/Client/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Login from './Components/login/Login';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+    <Login />
     </div>
   );
 }
