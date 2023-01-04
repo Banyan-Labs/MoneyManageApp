2d0bf47933ece1cbd74fb8643fb2649315a195b6
JessGrover <120360096+jgrover921@users.noreply.github.com>
2023-01-04 14:07:59 -0700

    Revert "Push"

--- money-managed-app/src/App.js
+++ money-managed-app/src/App.js
@@ -1,26 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Layout from './Layout';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignIn from './SignIn'
-
-
-
 
 function App() {
   return (
-    <div>
-     <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Layout />}>
-              <Route index element={<SignIn />} />
-              <Route path="SignUp" element={<SignUp />} />
-              <Route path="ForgotPW" element={<ForgotPW />} />
-              {/* <Route path="" element={< />} />
-              <Route path="" element={< />} /> */}
-              {/* <Route path="*" element={<NoPage />} /> */}
-          </Route>
-      </Routes>
-  </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
