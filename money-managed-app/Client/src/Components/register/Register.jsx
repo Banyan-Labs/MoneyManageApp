e5f008fce49fe1c48d145820d8af889ab4da6b66
MalloryReed <gregorymallory89@gmail.com>
2023-01-06 09:52:51 -0700

    MM-11

--- money-managed-app/Client/src/Components/register/Register.jsx
+++ money-managed-app/Client/src/Components/register/Register.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "./Register.css";
+
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+     
+      <div className='logo-name'>Money Managed</div>
+    
+    <div className='inner-register-div'>
+      <h5>Create An Account</h5>
+  <input type='text' name='name' placeholder='First Name'required /><br /><br />
+  <input type='text' name='name' placeholder='Last Name'required /><br /><br />
+  <input type='email' name='email' placeholder='Email' required /><br /><br />
+  <input type='text' name='name' placeholder='Password'required /><br /><br />
+  <input type='text' name='name' placeholder='Confirm Password'required /><br /><br />
+  <button type='button' className='button'>Sign Up</button>
+  <p>Already have an account? <a href='#Register'>Sign In</a></p>
+</div>
+</div>
   )
 }
 
