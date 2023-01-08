8905e5e5b39eec11d22af1f4d9c74816ae9f25e6
Josh McLain <joshmclain45@gmail.com>
2023-01-08 13:24:29 -0700

    config linting back, fix front dev build

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const NODE_ENV = !process.env.NODE_ENV ? "development" : process.env.NODE_ENV;
+const NODE_ENV = !process.env.NODE_ENV ? 'development' : process.env.NODE_ENV;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
   res.end();
 });
 
